505e0fe7e05eb3532ac9abae48d8f1da2a90555b
dnadler <daniel_nadler@putnam.com>
2016-08-29 11:14:17 -0400

    time-series plotting with pandas. need to clear and redraw lines, currently adding new lines on top of old chart!

--- twitterStream/auth.py
+++ twitterStream/auth.py
@@ -1,6 +1,11 @@
 from twitter import oauth_dance, read_token_file, TwitterStream, OAuth
 import os
-from math import log, fabs, nan, acos, sin, cos, pi
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import pandas as pd
+import time
+from math import log, fabs, nan, acos, sin, cos, pi, inf
+
 CONSUMER_KEY = 'uqiCSPB5CYtMXYN4wV2LUkwiL'
 CONSUMER_SECRET = 'q3sSeTZNKKAc4eladDvyVlyDctL2066ht36wpwjYjpByLgWKQJ'
 
@@ -16,9 +21,21 @@ iterartor = twitter_stream.statuses.filter(track='trump')
 stop_list = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
 exclude_words = ['hillary','trump','donald','about','&amp;']
 
+style.use('fivethirtyeight')
+fig = plt.figure(figsize=(10,10))
+ax = plt.gca()
+plt.ion()
+fig.canvas.draw()
+
+numwords = 8
+
+df = pd.DataFrame()
+
+
 print('Streaming...', end='\r')
 words = dict()
 j = 0
+xi = 0
 for tweet in iterartor:
     j += 1
     # Count words
@@ -45,6 +62,7 @@ for tweet in iterartor:
             word = word.replace('\t', '')
             word = word.replace( '?', '')
             word = word.replace( '&', '')
+            word = word.replace( '"', '')
 
             if word in exclude_words:
                 continue
@@ -55,7 +73,11 @@ for tweet in iterartor:
             cleaned_list.append(word)
 
         for word in cleaned_list:
+            temp_df = pd.DataFrame(index=[pd.datetime.now()], columns=[word], data=1)
+            df = df.append(temp_df)
+
             if word in words:
+
                 words[word]['count'] += 1
             else:
                 words[word] = dict()
@@ -76,31 +98,68 @@ for tweet in iterartor:
     except:
         pass
 
+
+
+
     if j % 500 == 0:
         j = 0
         words = dict()
 
-    if j > 250:
-        # Order words by count
-        vect = sorted([(word, subdict['count'], subdict['intersect']) for word, subdict in words.items() if 'intersect' in subdict], key=lambda x: -x[1])
-        strout = ''
-        for k in range(8):
-            strout += '{0}: {1}  '.format(vect[k][0], vect[k][1])
-        print(strout, end='\r')
+    if j > 20:
 
-        # Calculate distances of top 10 words
-        distance_map = dict()
-        for k in range(10):
-            v = vect[k]
-            distance_map[v[0]] = dict()
-            for word, count in v[2].items():
+        df = df.resample('S').sum().replace(nan, 0)
+        df_rolling = df.rolling(360, min_periods=3).sum().T.sort_values(df.resample('S').index.values[-1])
+
+        # Order words by count
+        # vect = sorted([(word, subdict['count'], subdict['intersect']) for word, subdict in words.items() if 'intersect' in subdict], key=lambda x: -x[1])
+        # strout = ''
+        # for k in range(numwords):
+        #     strout += '{0}: {1}  '.format(vect[k][0], vect[k][1])
+        # print(strout, end='\r')
+        #
+        # # Calculate distances of top 10 words
+        # distance_map = dict()
+        # for k in range(10):
+        #     v = vect[k]
+        #     distance_map[v[0]] = dict()
+        #     for word, count in v[2].items():
+        #         try:
+        #             distance = fabs(1/log(count))
+        #             distance_map[v[0]][word] = distance
+        #         except:
+        #             distance = nan
+
+        # Plot time-series
+        df_plot = df_rolling.tail(numwords).T
+        df_plot.plot(ax=ax, legend=False)
+
+        # try:
+        #     for t in ax.texts:
+        #         t.remove()
+        #         t.set_visible(False)
+        # except:
+        #     pass
+
+        if len(df.index.values) % 20 == 0:
+            li = 0
+            for line in ax.lines:
                 try:
-                    distance = fabs(1/log(count))
-                    distance_map[v[0]][word] = distance
+                    x = ax.get_axes().dataLim._get_max()[0]
+                    y = df_plot.ix[-1,li]
+                    s = df_plot.columns.values[li]
+                    li += 1
+                    if y != nan:
+                        if x != inf:
+                            ax.text(x,y,s)
                 except:
-                    distance = nan
+                    pass
+
+        ax.relim()
+        ax.autoscale_view(True, True, True)
+        plt.pause((0.01))
 
         # Convert distance to XY coordinates
+        continue
         coords = dict()
         for centroid, words_dict in distance_map.items():
             coords[centroid] = dict()
